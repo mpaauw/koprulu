80a2993a64d26d58ffc0afa06948ba43dba09616
mpaauw <matt.paauw@gmail.com>
2019-06-03 20:22:00 -0700

    Update server info.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Planet = require('./api/models/planet'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/koprulu');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/planetsRoutes');
+routes(app);
 
 app.listen(port);
 
